7a3e967b01134e71e3a3e401e0379ceaaed058f7
devanshuchauhan1999 <chauhandevanshu121@gmail.com>
2022-03-30 00:30:31 +0530

    data binding for ListFragment is Done

--- app/src/main/java/com/devanshu/todoapp/fragments/list/ListAdapter.kt
+++ app/src/main/java/com/devanshu/todoapp/fragments/list/ListAdapter.kt
@@ -1,65 +1,55 @@
 package com.devanshu.todoapp.fragments.list
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
-import androidx.navigation.findNavController
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.devanshu.todoapp.R
-import com.devanshu.todoapp.data.models.Priority
 import com.devanshu.todoapp.data.models.ToDoData
 import com.devanshu.todoapp.databinding.RowLayoutBinding
-import kotlinx.android.synthetic.main.row_layout.view.*
+
 
 class ListAdapter : RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
 
     var dataList = emptyList<ToDoData>()
 
+    class MyViewHolder(private val binding: RowLayoutBinding) : RecyclerView.ViewHolder(binding.root){
 
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {}
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        return MyViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.row_layout, parent, false)
-        )
+        fun bind(toDoData: ToDoData){
+            binding.toDoData = toDoData
+            binding.executePendingBindings()
+        }
+        companion object{
+            fun from(parent: ViewGroup): MyViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = RowLayoutBinding.inflate(layoutInflater, parent, false)
+                return MyViewHolder(
+                    binding
+                )
+            }
+        }
     }
 
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        //Depreciated content
-        holder.itemView.title_txt.text = dataList[position].title
-        holder.itemView.description_txt.text = dataList[position].description
-        holder.itemView.row_background.setOnClickListener {
-            val action  = ListFragmentDirections.actionListFragmentToUpdateFragment(dataList[position])
-            holder.itemView.findNavController().navigate(action)
-        }
-
-        when (dataList[position].priority) {
-            Priority.HIGH -> holder.itemView.priority_indicator.setCardBackgroundColor(
-                ContextCompat.getColor(
-                    holder.itemView.context,
-                    R.color.red
-                )
-            )
-            Priority.MEDIUM -> holder.itemView.priority_indicator.setCardBackgroundColor(
-                ContextCompat.getColor(holder.itemView.context, R.color.yellow)
-            )
-            Priority.LOW -> holder.itemView.priority_indicator.setCardBackgroundColor(
-                ContextCompat.getColor(
-                    holder.itemView.context,
-                    R.color.green
-                )
-            )
-        }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder.from(
+            parent
+        )
     }
 
     override fun getItemCount(): Int {
         return dataList.size
     }
 
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentItem = dataList[position]
+        holder.bind(currentItem)
+    }
 
     fun setData(toDoData: List<ToDoData>){
+        //val toDoDiffUtil = ToDoDiffUtil(dataList, toDoData)
+        //val toDoDiffResult = DiffUtil.calculateDiff(toDoDiffUtil)
         this.dataList = toDoData
         notifyDataSetChanged()
+        //toDoDiffResult.dispatchUpdatesTo(this)
     }
+
 }
--- app/src/main/java/com/devanshu/todoapp/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/devanshu/todoapp/fragments/update/UpdateFragment.kt
@@ -22,29 +22,30 @@ class UpdateFragment : Fragment() {
     private val args by navArgs<UpdateFragmentArgs>()
 
     private val mSharedViewModel: SharedViewModel by viewModels()
-
     private val mToDoViewModel: ToDoViewModel by viewModels()
 
-    private var _binding: FragmentUpdateBinding?= null
-    private val binding get()= _binding
+    private var _binding: FragmentUpdateBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        // Data binding
+        _binding = FragmentUpdateBinding.inflate(inflater, container, false)
+        binding.args = args
 
-        // Data Binding
-        _binding = FragmentUpdateBinding.inflate(inflater,container,false)
-        binding?.args = args
-
-        //set menu
+        // Set Menu
         setHasOptionsMenu(true)
 
-        //Spinner Item Selected Listener
-        binding?.currentPrioritiesSpinner?.onItemSelectedListener = mSharedViewModel.listener
+        // Spinner Item Selected Listener
+        binding.currentPrioritiesSpinner.onItemSelectedListener = mSharedViewModel.listener
 
-        return view
+        return binding.root
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.update_fragment_menu, menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -55,49 +56,46 @@ class UpdateFragment : Fragment() {
         return super.onOptionsItemSelected(item)
     }
 
-    //for updating data
     private fun updateItem() {
-        val title = current_title_et.text.toString()
-        val description = current_description_et.text.toString()
-        val getPriority = current_priorities_spinner.selectedItem.toString()
+        val title = binding.currentTitleEt.text.toString()
+        val description = binding.currentDescriptionEt.text.toString()
+        val getPriority = binding.currentPrioritiesSpinner.selectedItem.toString()
 
         val validation = mSharedViewModel.verifyDataFromUser(title, description)
         if (validation) {
-            val updateItem = ToDoData(
+            // Update Current Item
+            val updatedItem = ToDoData(
                 args.currentItem.id,
                 title,
                 mSharedViewModel.parsePriority(getPriority),
                 description
             )
-            mToDoViewModel.updateData(updateItem)
-            Toast.makeText(requireContext(), "Successfully Updated", Toast.LENGTH_SHORT).show()
+            mToDoViewModel.updateData(updatedItem)
+            Toast.makeText(requireContext(), "Successfully updated!", Toast.LENGTH_SHORT).show()
+            // Navigate back
             findNavController().navigate(R.id.action_updateFragment_to_listFragment)
         } else {
-            Toast.makeText(requireContext(), "Please fill all fields", Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), "Please fill out all fields.", Toast.LENGTH_SHORT)
+                .show()
         }
     }
 
-    //Alert dialog for confirm removal
+    // Show AlertDialog to Confirm Item Removal
     private fun confirmItemRemoval() {
         val builder = AlertDialog.Builder(requireContext())
         builder.setPositiveButton("Yes") { _, _ ->
             mToDoViewModel.deleteItem(args.currentItem)
             Toast.makeText(
                 requireContext(),
-                "Successfully Removed : ${args.currentItem.title}",
+                "Successfully Removed: ${args.currentItem.title}",
                 Toast.LENGTH_SHORT
             ).show()
             findNavController().navigate(R.id.action_updateFragment_to_listFragment)
         }
         builder.setNegativeButton("No") { _, _ -> }
-        builder.setTitle("Delete '${args.currentItem.title}'")
-        builder.setMessage("Are you sure want to remove '${args.currentItem.title}'")
+        builder.setTitle("Delete '${args.currentItem.title}'?")
+        builder.setMessage("Are you sure you want to remove '${args.currentItem.title}'?")
         builder.create().show()
-    }
-
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.update_fragment_menu, menu)
     }
 
     override fun onDestroyView() {
